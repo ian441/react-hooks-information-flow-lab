a3e51dff4eb04818a9322baa12338ef4ee825e69
ian ayiro <ian.ayiro@student.moringa.com>
2024-10-13 22:37:30 +0300

    modified:   src/components/Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header({ isDarkMode, onDarkModeClick }) {
     <header>
         <h2>Shopster</h2>
         <button onClick={onDarkModeClick}>{isDarkMode ? 'Dark' : 'Light'}
-            Mode
+        MODE
         </button>
     </header>
   )
