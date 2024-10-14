2f5f8f05136243e746ce4dd00741dff36e1bf150
ian ayiro <ian.ayiro@student.moringa.com>
2024-10-14 21:48:47 +0300

    modified:   src/components/Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 function Header({ isDarkMode, onDarkModeClick }) {
+  let modeText; if(isDarkMode) {
+    modeText= 'Dark';
+  }else{
+    modeText= 'Light';
+  }
   return (
     <header>
         <h2>Shopster</h2>
-        <button onClick={onDarkModeClick}>{isDarkMode ? 'Dark' : 'Light'}
+        <button onClick={onDarkModeClick}>{modeText}
         MODE
         </button>
     </header>
